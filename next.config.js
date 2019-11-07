8fad59cb5ea35e8403cf2e20d2135a0fb05f3b71
wiasliaw77210 <leewei48750@gmail.com>
2019-11-08 00:58:07 +0800

    feat(next.config.js): feat webpack config for image

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withImages = require('next-images');
+const PROJ_NAME = 'digital-certificate-ncku';
+const GITHUB = process.env.DEPLOY_ENV === 'github';
+
+module.exports = withImages({
+  assetPrefix: GITHUB ? `/${PROJ_NAME}/` : '',
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      index: { page: '/index' },
+      support: { page: '/support' },
+      upload: { page: '/upload' },
+    };
+  },
+  webpack: function(config) {
+    return config;
+  },
+});

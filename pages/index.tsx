5ebe0fe662ff4dc9708d5025eb1866fa1845214a
wiasliaw77210 <leewei48750@gmail.com>
2019-11-08 01:00:24 +0800

    feat(pages/): add _document and remove enum to constants

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Router from 'next/router';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import Nav from '../components/nav';
 
 const Index = () => {
-  const route = useRouter();
+  useEffect(() => {
+    Router.push('/upload');
+  }, []);
   return (
     <div>
       <Head>
         <title>Home</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Nav />
-      <style jsx global>{`
-        html,
-        body {
-          margin: 0;
-          background-color: #f5f5f5;
-        }
-      `}</style>
     </div>
   );
 };

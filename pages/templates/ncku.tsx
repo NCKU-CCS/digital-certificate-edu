f2f8e5e2eaadfb771239f0f1b5c543c54088e16b
Wei Lee <leewei48750@gmail.com>
2019-12-02 18:22:09 +0800

    feat(ncku.tsx): feat new style

--- pages/templates/ncku.tsx
+++ pages/templates/ncku.tsx
@@ -3,6 +3,56 @@ import styled from 'styled-components';
 import TemplateImage from '../../static/templates/ncku.jpg';
 import A4 from '../../layouts/A4';
 
+enum EType {
+  bachelor,
+  master,
+}
+
+interface IField {
+  // student
+  studentName: string;
+  studentNation?: string;
+  studentBirth: string;
+  studentId: string;
+  college: string;
+  department: string;
+  degree: string;
+  // double_major
+  dm_college?: string;
+  dm_department?: string;
+  dm_degree?: string;
+  // minor
+  minor?: string;
+  // other
+  title: string;
+  date: string;
+  digit_id: string;
+  cert_id: string;
+  type: EType;
+}
+
+const field: IField = {
+  // student
+  studentName: 'xxx',
+  studentNation: '香港',
+  studentId: 'F129391835',
+  studentBirth: '八十六年七月三十一日生',
+  college: '生物科學與科技學院',
+  department: '生物科技與產業科學學系學士班',
+  degree: '理學學士',
+  // double major & minor
+  dm_college: '文學院',
+  dm_department: '中國文學系學士班',
+  dm_degree: '文學學士',
+  minor: '化學工程學系',
+  // other
+  title: '學士學位證書',
+  date: '中華民國 108 年 1 月',
+  digit_id: '數位字樣',
+  cert_id: '(108)成大字第 1111 號',
+  type: EType.bachelor,
+};
+
 const Title = styled.h1`
   font-size: 28px;
   margin-top: 100px;
@@ -11,9 +61,11 @@ const Title = styled.h1`
   letter-spacing: 3px;
 `;
 
-const SubTitle = styled.p`
-  font-size: 16px;
-  text-align: right;
+const SubTitle = styled.div`
+  width: 100%;
+  font-size: 15px;
+  display: flex;
+  flex-direction: row-reverse;
 `;
 
 const Text = styled.p`
@@ -27,41 +79,85 @@ const Date = styled(Text)`
   position: absolute;
   bottom: 80px;
   letter-spacing: 3px;
+  font-size: 15px;
 `;
 
-const field = {
-  title: '學士學位證書',
-  subTitle: '學士學位證書',
-  number: '42488',
-  studentNumber: 'F129391835',
-  studentName: 'xxx',
-  studentBirthday: '八十六年七月三十一日生',
-  school: '生物科學與科技學院',
-  department: '生物科技與產業科學學系學士班',
-  studentTitle: '理學學士',
-  createdYear: '108',
-  createdMonth: '06',
-};
-
 const Template = () => {
+  const format = (input: string) => {
+    const output: string[] = [];
+    let temp = '';
+    for (const c of input) {
+      temp = temp.concat(c);
+      if (temp.length % 16 === 0) {
+        output.push(temp);
+        temp = '';
+      }
+    }
+    output.push(temp);
+    return output;
+  };
   return (
     <A4 title={field.title} backgroundUri={TemplateImage}>
       <Title>{field.title}</Title>
-      <SubTitle>(108)成大字第{field.number}號</SubTitle>
-      <SubTitle style={{ marginBottom: '30px' }}>
-        {field.studentNumber}
+      <SubTitle>
+        <p>
+          {field.cert_id}
+          <br />
+          學號：{field.studentId}
+        </p>
       </SubTitle>
-      <Text>學生 {field.studentName}</Text>
-      <Text>中華民國{field.studentBirthday}</Text>
-      <Text>在本校&nbsp;&nbsp;{field.school}</Text>
-      <Text>{field.department}</Text>
-      <Text>休業期滿成績及格准予畢業依學位授予法</Text>
-      <Text>之規定授予{field.studentTitle}學位 &nbsp;&nbsp;此證</Text>
-
-      <Text style={{ marginTop: '20px' }}>校長</Text>
+      {/* 1 */}
+      <Text>
+        學生　{field.studentName}
+        {field.studentNation ? `　　　${field.studentNation}` : ''}
+      </Text>
+      {/* 2 */}
+      <Text>{field.studentBirth}</Text>
+      {/* 3 */}
+      <Text>在本校　　{field.college}</Text>
+      {/* 4, 5 */}
+      {EType.bachelor === field.type ? (
+        <React.Fragment>
+          <Text>{field.department}</Text>
+          {format(
+            `修業期滿成績及格准予畢業依學位授予法之規定授予${field.degree}此證`,
+          ).map((e, i) => (
+            <Text key={i}>{e}</Text>
+          ))}
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          {format(
+            `${field.department}　研究期滿經碩士學位考試合格依學位授予法之規定授予　　${field.degree}　　`,
+          ).map((e, i) => (
+            <Text key={i}>{e}</Text>
+          ))}
+          <Text>此證</Text>
+        </React.Fragment>
+      )}
+      {/* 6, 7, 8 */}
+      {field.dm_college ? (
+        <React.Fragment>
+          <Text>
+            雙主修：{field.dm_college}　{field.dm_department}
+          </Text>
+          <Text>　　　　授予　{field.dm_degree}　學位</Text>
+        </React.Fragment>
+      ) : (
+        <></>
+      )}
+      {field.minor ? (
+        <React.Fragment>
+          <Text>輔系：{field.minor}</Text>
+        </React.Fragment>
+      ) : (
+        <></>
+      )}
+      {/* footer */}
       <Date>
-        中華民國 &nbsp;&nbsp; {field.createdYear} &nbsp;&nbsp;年&nbsp;&nbsp;{' '}
-        {field.createdMonth} &nbsp;&nbsp;月
+        {field.date}
+        <br />
+        {field.digit_id}
       </Date>
     </A4>
   );

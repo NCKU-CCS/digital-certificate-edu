749723074e220a848c973536709d09c5ed9f8653
Wei Lee <leewei48750@gmail.com>
2019-12-18 23:29:01 +0800

    feat(./): add lenguage detector

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import detector from 'i18next-browser-languagedetector';
 import resourceEn from './en.json';
 import resourceCh from './ch.json';
 
@@ -12,13 +13,16 @@ const resources = {
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'ch',
-  interpolation: {
-    escapeValue: false,
-  },
-  keySeparator: false,
-});
+i18n
+  .use(initReactI18next)
+  .use(detector)
+  .init({
+    resources,
+    lng: 'ch',
+    interpolation: {
+      escapeValue: false,
+    },
+    keySeparator: false,
+  });
 
 export default i18n;

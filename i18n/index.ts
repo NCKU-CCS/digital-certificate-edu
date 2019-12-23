4b9e5b62db4f383ac9be5e6137d4b6b1c2e1f922
Wei Lee <leewei48750@gmail.com>
2019-12-23 13:20:08 +0800

    fix(./): use language detector

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,24 +1,28 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import resourceEn from './en.json';
-import resourceCh from './ch.json';
+import resourceZh from './zh.json';
 
 const resources = {
-  ch: {
-    translation: resourceCh,
+  zh: {
+    translation: resourceZh,
   },
   en: {
     translation: resourceEn,
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'ch',
-  interpolation: {
-    escapeValue: false,
-  },
-  keySeparator: false,
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'zh',
+    interpolation: {
+      escapeValue: false,
+    },
+    keySeparator: false,
+  });
 
 export default i18n;
